05892ca6bed2a5b32ab4d9935601f02643368af6
chuckcubed <chuckcubed@8d2b6eae-2fe3-11de-ba44-d58fe8a9ce33>
2009-05-28 01:24:29 +0000

    whitespace

--- trunk/src/boardrater/FinalRater.java
+++ trunk/src/boardrater/FinalRater.java
@@ -47,7 +47,7 @@ public class FinalRater extends BoardRater {
    
  public FinalRater() {
    // System.out.println("new final rater:");
-   // String temp;
+   // String temp;`
    // for(int i=0; i<raters.length; i++) {
    //   System.out.println((temp=""+coefficients[i]).substring(0,temp.length()>=4?temp.length():3)+"\t\t"+raters[i]);
    // }
